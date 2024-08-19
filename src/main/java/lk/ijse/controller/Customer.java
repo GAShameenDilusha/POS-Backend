45dda6c8593c27aec7e4246bf9f728f4ff5de2de
shameen <shameendilusha5678@gmail.com>
2024-08-19 15:56:57 +0530

    Webservlet annotation added

--- src/main/java/lk/ijse/controller/Customer.java
+++ src/main/java/lk/ijse/controller/Customer.java
@@ -1,4 +1,35 @@
 package lk.ijse.controller;
 
+import jakarta.json.Json;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.assignment_11_backend.db.DBProcess;
+import lk.ijse.assignment_11_backend.dto.CustomerDTO;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name ="customer" ,urlPatterns = "/customer",
+        initParams = {
+                //create the path for the database connection...
+                @WebInitParam(name = "db-user",value = "root"),
+                @WebInitParam(name = "db-pw",value = "1234"),
+                @WebInitParam(name = "db-url",value = "jdbc:mysql://localhost:3306/pos?createDatabaseIfNotExist=true"),
+                @WebInitParam(name = "db-class",value = "com.mysql.cj.jdbc.Driver")
+
+        })
+
 public class Customer {
 }
