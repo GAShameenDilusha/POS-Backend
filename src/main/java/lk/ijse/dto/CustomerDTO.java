6cf6e3b19fc7bcf5d669cefa3975d04dd8d918f3
shameen <shameendilusha5678@gmail.com>
2024-08-20 08:41:26 +0530

    update CustomerDTO.java

--- src/main/java/lk/ijse/dto/CustomerDTO.java
+++ src/main/java/lk/ijse/dto/CustomerDTO.java
@@ -1,4 +1,20 @@
 package lk.ijse.dto;
 
-public class CustomerDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+//Follow the bean spec...
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CustomerDTO implements Serializable {
+    private String customer_id;
+    private String name;
+    private String address;
+    private String contact;
+
+
 }
