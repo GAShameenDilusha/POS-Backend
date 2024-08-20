69b9943875e1cb99f354ce3c3f6415a6acbda8e1
shameen <shameendilusha5678@gmail.com>
2024-08-20 08:41:52 +0530

    update ItemDTO.java

--- src/main/java/lk/ijse/dto/ItemDTO.java
+++ src/main/java/lk/ijse/dto/ItemDTO.java
@@ -1,4 +1,16 @@
 package lk.ijse.dto;
 
-public class ItemDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ItemDTO implements Serializable {
+    private String item_id;
+    private String descr;
+    private double price;
+    private int qty;
 }
