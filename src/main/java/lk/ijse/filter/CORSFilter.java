5182b50a7fb389dc6d5ff7dc78ffbf15fa12a936
shameen <shameendilusha5678@gmail.com>
2024-08-20 08:32:56 +0530

    update CORSFilter.java

--- src/main/java/lk/ijse/filter/CORSFilter.java
+++ src/main/java/lk/ijse/filter/CORSFilter.java
@@ -1,4 +1,31 @@
 package lk.ijse.filter;
 
-public class CORSFilter {
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class CORSFilter extends HttpFilter {
+    //Filter the methods to give access for the CORS origin policy...
+
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        System.out.println("CORS Filter");
+        String origin = req.getHeader("Origin");
+        if(origin.contains(getServletContext().getInitParameter("origin"))){
+            res.setHeader("Access-Control-Allow-Origin","*");
+            res.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,HEADER");
+            res.setHeader("Access-Control-Allow-Headers","Content-Type");
+            res.setHeader("Access-Control-Expose-Headers","Content-Type");
+
+
+        }
+        chain.doFilter(req,res);
+    }
 }
+
