002ebdd645ae0788c464683d0272b667d6d2a547
shameen <shameendilusha5678@gmail.com>
2024-08-20 08:42:20 +0530

    update OrderDetailsDTO.java

--- src/main/java/lk/ijse/dto/OrderDetailsDTO.java
+++ src/main/java/lk/ijse/dto/OrderDetailsDTO.java
@@ -1,4 +1,18 @@
 package lk.ijse.dto;
 
-public class OrderDetailsDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class OrderDetailsDTO implements Serializable {
+
+    private String order_id;
+    private String customer_id;
+    private String date;
+    private String total;
 }
