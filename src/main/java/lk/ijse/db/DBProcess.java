97df6c3e20e5d15fcfd94185e1aa8129a24c34c9
shameen <shameendilusha5678@gmail.com>
2024-08-21 11:35:39 +0530

    add quires

--- src/main/java/lk/ijse/db/DBProcess.java
+++ src/main/java/lk/ijse/db/DBProcess.java
@@ -1,4 +1,24 @@
 package lk.ijse.db;
 
 public class DBProcess {
+    private static final String SAVE_DATA = "INSERT INTO customer(CUSTOMER_ID,NAME,ADDRESS,CONTACT) VALUES (?,?,?,?)";
+    private static final String SAVE_ITEM_DATA = "INSERT INTO item(ITEM_ID, DESCR, PRICE, QTY) VALUES (?,?,?,?)";
+
+    private static final String GET_ALL_CUSTOMER_DATA = "SELECT * FROM customer";
+    private static final String UPDATE_CUSTOMER = "UPDATE customer SET name=? ,address=?, contact=? WHERE customer_id=?";
+    private static final String DELETE_CUSTOMER = "DELETE FROM customer WHERE customer_id=?;";
+    private static final String GET_ALL_ITEM_DATA = "SELECT * FROM item";
+    private static final String UPDATE_ITEM = "UPDATE item SET descr=? ,price=?, qty=? WHERE item_id=?";
+
+    private static final String DELETE_ITEM = "DELETE FROM item WHERE item_id=?;";
+
+    private static final String SAVE_ORDER = "INSERT INTO order_details(order_id,customer_id,date,total) VALUES(?,?,?,?);";
+
+    private static final String SAVE_ORDER_DETAILS = "INSERT INTO order_item(order_id,item_id,qty) VALUES(?,?,?);";
+
+    private static final String GET_ALL_ORDERS = "SELECT * FROM order_details;";
+
+
+    final static Logger logger = LoggerFactory.getLogger(DBProcess.class);
+
 }
