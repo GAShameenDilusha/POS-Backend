3581ca18af77ae4796c86d1a44632cfcff799702
shameen <shameendilusha5678@gmail.com>
2024-08-23 15:55:16 +0530

    remove the method from Transaction.java

--- src/main/java/lk/ijse/db/Transaction.java
+++ src/main/java/lk/ijse/db/Transaction.java
@@ -6,27 +6,5 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Transaction {
-    public void orderTransaction(OrderDTO orderDTO, Connection connection) throws SQLException {
-        connection.setAutoCommit(false);
 
-        try {
-            var dbProcess = new lk.ijse.db.DBProcess();
-            if (dbProcess.saveOrder(orderDTO, connection)) {
-                if (dbProcess.saveOrderDetails(orderDTO, connection)) {
-                    connection.setAutoCommit(true);
-                } else {
-                    connection.rollback();
-                }
-            } else {
-                connection.rollback();
-
-            }
-        } catch (SQLException e) {
-            connection.rollback();
-            throw e;
-        } finally {
-            connection.setAutoCommit(true);
-        }
-
-    }
 }
