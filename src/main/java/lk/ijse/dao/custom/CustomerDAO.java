6b9dda666e35722b67f138e8be5eb58c30b4d08a
shameen <shameendilusha5678@gmail.com>
2024-08-23 15:50:47 +0530

    update CustomerDAO.java

--- src/main/java/lk/ijse/dao/custom/CustomerDAO.java
+++ src/main/java/lk/ijse/dao/custom/CustomerDAO.java
@@ -1,4 +1,20 @@
 package lk.ijse.dao.custom;
 
+import lk.ijse.dao.impl.CustomerDAOImpl;
+import lk.ijse.dto.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public interface CustomerDAO {
+     String saveCustomer(CustomerDTO customerDTO, Connection connection) ;
+
+     List<CustomerDTO> getCustomerData(Connection connection) throws SQLException;
+
+     String updateCustomer(CustomerDTO customerDTO, Connection connection);
+
+     String deleteCustomer(CustomerDTO customerDTO, Connection connection);
+
 }
