fafbd99785ca1be2b665dfa54c845973a1240729
shameen <shameendilusha5678@gmail.com>
2024-08-23 15:55:48 +0530

    Update OrderDAO interface

--- src/main/java/lk/ijse/dao/custom/OrderDAO.java
+++ src/main/java/lk/ijse/dao/custom/OrderDAO.java
@@ -1,5 +1,22 @@
 package lk.ijse.dao.custom;
 
+import lk.ijse.dto.ItemDTO;
+import lk.ijse.dto.OrderDTO;
+import lk.ijse.dto.OrderDetailsDTO;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public interface OrderDAO {
+     boolean saveOrder(OrderDTO orderDTO, Connection connection);
+
+     boolean saveOrderDetails(OrderDTO orderDTO, Connection connection);
+
+     List<OrderDetailsDTO> getAllOrders(Connection connection) throws SQLException;
+
+     void orderTransaction(OrderDTO orderDTO, Connection connection) throws SQLException;
 
 }
