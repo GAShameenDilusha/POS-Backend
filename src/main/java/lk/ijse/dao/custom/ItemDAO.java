eacaea9dcd0a90802c71e634f89f249684d858c6
shameen <shameendilusha5678@gmail.com>
2024-08-23 15:53:56 +0530

    ItemDAO interface updated

--- src/main/java/lk/ijse/dao/custom/ItemDAO.java
+++ src/main/java/lk/ijse/dao/custom/ItemDAO.java
@@ -1,4 +1,21 @@
 package lk.ijse.dao.custom;
 
+import lk.ijse.dto.ItemDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public interface ItemDAO {
+     String saveItem(ItemDTO itemDTO, Connection connection);
+
+     List<ItemDTO> getItemData(Connection connection) throws SQLException;
+
+     String updateItem(ItemDTO itemDTO, Connection connection);
+
+     String deleteItem(ItemDTO itemDTO, Connection connection);
+
+
 }
