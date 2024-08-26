a26430b1d6a3fedcbfe20169c355e61488ccdae1
shameen <shameendilusha5678@gmail.com>
2024-08-26 13:47:29 +0530

    update CustomerBO.java

--- src/main/java/lk/ijse/bo/custom/CustomerBO.java
+++ src/main/java/lk/ijse/bo/custom/CustomerBO.java
@@ -1,4 +1,18 @@
 package lk.ijse.bo.custom;
 
+import lk.ijse.dto.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface CustomerBO {
+    String saveCustomer(CustomerDTO customerDTO, Connection connection) ;
+
+    List<CustomerDTO> getCustomerData(Connection connection) throws SQLException;
+
+    String updateCustomer(CustomerDTO customerDTO, Connection connection);
+
+    String deleteCustomer(CustomerDTO customerDTO, Connection connection);
+
 }
