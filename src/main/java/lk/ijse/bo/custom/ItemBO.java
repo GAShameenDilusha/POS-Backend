839e5705e50b01e8b41e22c0fb310c1061605e0a
shameen <shameendilusha5678@gmail.com>
2024-08-26 13:48:05 +0530

    update ItemBO.java

--- src/main/java/lk/ijse/bo/custom/ItemBO.java
+++ src/main/java/lk/ijse/bo/custom/ItemBO.java
@@ -1,4 +1,17 @@
 package lk.ijse.bo.custom;
 
+import lk.ijse.dto.ItemDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ItemBO {
+    String saveItem(ItemDTO itemDTO, Connection connection);
+
+    List<ItemDTO> getItemData(Connection connection) throws SQLException;
+
+    String updateItem(ItemDTO itemDTO, Connection connection);
+
+    String deleteItem(ItemDTO itemDTO, Connection connection);
 }
