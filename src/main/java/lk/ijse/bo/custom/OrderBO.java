5a0808917c763f34fa067c07063a955277cf24b5
shameen <shameendilusha5678@gmail.com>
2024-08-26 13:48:41 +0530

    update OrderBO.java

--- src/main/java/lk/ijse/bo/custom/OrderBO.java
+++ src/main/java/lk/ijse/bo/custom/OrderBO.java
@@ -1,4 +1,18 @@
 package lk.ijse.bo.custom;
 
+import lk.ijse.dto.OrderDTO;
+import lk.ijse.dto.OrderDetailsDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface OrderBO {
+    boolean saveOrder(OrderDTO orderDTO, Connection connection);
+
+    boolean saveOrderDetails(OrderDTO orderDTO, Connection connection);
+
+    List<OrderDetailsDTO> getAllOrders(Connection connection) throws SQLException;
+
+    void orderTransaction(OrderDTO orderDTO, Connection connection) throws SQLException;
 }
